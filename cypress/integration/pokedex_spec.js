cddda3793ea64751d4509b11f7bfd5c94e27b648
Bear Sterns <bearsterns.dev@gmail.com>
2021-11-28 01:51:27 +0530

    test: links navigate to correct content

--- cypress/integration/pokedex_spec.js
+++ cypress/integration/pokedex_spec.js
@@ -1,7 +1,23 @@
 describe('Pokedex', function () {
+  beforeEach(
+    function () {
+      cy.visit('http://localhost:5000')
+    }
+  )
+
   it('front page can be opened', function () {
-    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('right content is loaded', function () {
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.get('.pokemon-ability-name')
+      .should('have.length', 2)
+      .then(abilities => {
+        cy.wrap(abilities[0]).contains('overgrow')
+        cy.wrap(abilities[1]).contains('chlorophyll')
+      })
   })
 })
