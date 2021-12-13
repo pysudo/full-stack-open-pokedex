b42d0e13c698a9da010b5fb59eb2c3c52e041667
Bear Sterns <bearsterns.dev@gmail.com>
2021-12-13 23:56:13 +0530

    deployment pipleline: health check

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+
+app.get('/version', (req, res) => {
+  res.send(process.env.HEROKU_RELEASE_VERSION)
+})
+
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
